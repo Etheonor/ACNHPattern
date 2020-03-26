45feb67613c91406eaee6d89c198709623de75b4
Mica <etheanore@gmail.com>
2020-03-26 23:48:44 +0100

    Add contact page/form

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const Footer = () => {
-	return (<footer><div>© {new Date().getFullYear()}, Sweetter / <Link to='/about/'>About</Link></div></footer>);
+	return (<footer><div>© {new Date().getFullYear()}, Sweetter / <Link to='/about/'>About</Link> / <Link to='/contact/'>Contact me</Link></div></footer>);
 };
 
 export default Footer;
---
+++ src/pages/contact.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+import petimg from './../images/pets.png';
+
+const Contact = () => (
+	<Layout>
+		<SEO title="Contact" />
+		<h1>Contact page</h1>
+        <p>You want more information about Sweetter ? Please feel free to contact me with this form!</p>
+		<form name="contact" method="POST" data-netlify-recaptcha="true">
+			<p>
+				<label>
+					Your Name: <input type="text" name="name" />
+				</label>
+			</p>
+			<p>
+				<label>
+					Your Email: <input type="email" name="email" />
+				</label>
+			</p>
+			<p>
+				<label>
+					Message: <textarea name="message" />
+				</label>
+			</p>
+			<p>
+				<button type="submit">Send</button>
+			</p>
+		</form>
+
+		<img src={petimg} alt="cute pets" />
+		<Link to="/">Go back to the homepage</Link>
+	</Layout>
+);
+
+export default Contact;
