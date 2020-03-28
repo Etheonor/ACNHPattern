7d1cefdc9800539316bf5103f3cfda202bac7236
Mica <etheanore@gmail.com>
2020-03-28 16:31:18 +0100

    update donate (custom button) and create thanks page

--- src/components/buttons/donate.js
+++ src/components/buttons/donate.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styles from './donate.module.css'
+import donate from './../../images/donate.png'
 
 const Donate = () => {
-	return (<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+	return (<form className={styles.button} action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 	<input type="hidden" name="cmd" value="_s-xclick" />
 	<input type="hidden" name="hosted_button_id" value="CNKW526N4MUC4" />
-	<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="Make a Paypal donation to help Sweetter!" alt="Donate with PayPal button" />
+	<input  type="image" src={donate} border="0" name="submit" title="Make a Paypal donation to help Sweetter!" alt="Donate with PayPal button" />
 	<img alt="" border="0" src="https://www.paypal.com/en_FR/i/scr/pixel.gif" width="1" height="1" />
 	</form>);
 };
---
+++ src/pages/thanks.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import heart from './../images/heart.png'
+
+const Thanks = () => (
+  <Layout>
+    <SEO title="Thanks for your donation" />
+    <h1>Thank you</h1>
+    <img style={{textAlign:'center'}} src={heart} alt='heart'/>
+    <p>Your donation is making a difference and will help me to improve Sweetter!</p>
+    
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Thanks
