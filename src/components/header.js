e77b777e6a457e166be3abe734c2dbe3c38c1f9f
Mica <etheanore@gmail.com>
2020-03-29 14:21:17 +0200

    Improved design (logo/main/bg color)

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,17 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import logo from './../images/sweetter-logo.png';
 import styles from './header.module.css';
 import Menu from './menu';
 
 const Header = ({ siteTitle, siteDescription }) => (
 	<header className={styles.header}>
 		<div className={styles.title}>
-			<img className={styles.logo} src={logo} alt="Sweetter logo" />
 			<h1>
 				<Link to="/" className={styles.link}>
 					{siteTitle}
 				</Link>
 			</h1>
-			<h2>{siteDescription}</h2>
 		</div>
 		<Menu className={styles.menu} />
 	</header>
