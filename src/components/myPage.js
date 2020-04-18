59ea35355f33ba79b3674f8753b2c5a5fef1909f
Michael <etheanore@gmail.com>
2020-04-18 17:39:41 +0200

    Favorites finally working

--- src/components/myPage.js
+++ src/components/myPage.js
@@ -1,62 +1,73 @@
-import React, { useState, useEffect } from "react";
-import styles from "./searchDesigner.module.scss";
+import React, { useState, useEffect, useContext } from "react";
+import styles from "./myPage.module.scss";
 import { firebase } from "../API/Firebase";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import PatternCard from "./patternCard";
-import Button from "./buttons/button";
+import PatternCard from "../components/patternCard";
 
+import {
+  GlobalDispatchContext,
+  GlobalStateContext,
+} from "../context/GlobalContextProvider";
 const db = firebase.firestore();
 const user = firebase.auth().currentUser;
 
 const MyPageComp = props => {
-  const [cards, setCards] = useState([]);
-  const [test, setTest] = useState(false)
+  const dispatch = useContext(GlobalDispatchContext);
+  const state = useContext(GlobalStateContext);
+  const [newCards, setNewCards] = useState([]);
+  const [test, setTest] = useState([]);
 
   useEffect(
     () => {
-      firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-          display(user);
-          console.log('test')
-        } else {
-        }
-      });
+      if (state.user) {
+        display();
+      }
     }, // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
+    [state.user]
   );
 
-  const display = user => {
-    let newState = [...cards];
-    db.collection("Users")
-      .doc(user.uid)
+  const display = () => {
+    let newState = [...newCards];
+    db.collection("UserPatterns")
+      .where("likes", "array-contains", state.user.uid)
+      .orderBy("likeCount", "desc")
       .get()
       .then(result => {
-        return result.data().likes;
-      })
-      .then(result => {
-        result.map(el => {
-          db.collection("UserPatterns")
-            .doc(el)
-            .get()
-            .then(result => {
-              newState.push(result.data());
-              newState[newState.length - 1].id = result.id;
-              return newState;
-            });
+        result.forEach(function(doc) {
+          // doc.data() is never undefined for query doc snapshots
+          newState.push(doc.data());
+          newState[newState.length - 1].id = doc.id;
         });
-        return newState;
-      })
-      .then(result => {
-        setCards(result);
-      }).then(() => {
-        setTest(true)
+        setNewCards(newState);
       });
   };
 
   return (
     <div>
-        <h2>My page</h2>
+      <h2>💕 My Favorites</h2>
+      <div className={styles.displayPatterns}>
+        {newCards ? (
+          newCards.map((value, index) => {
+            return (
+              <PatternCard
+                key={index}
+                user={value.user}
+                creatorCode={value.creatorCode}
+                designCode={value.designCode}
+                patternImage={value.patternImage}
+                designName={value.designName}
+                likes={value.likes}
+                desc={value.description}
+                likeCount={value.likeCount}
+                object={value.id}
+              />
+            );
+          })
+        ) : (
+          <p>TEST</p>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/myfavorites.js
+++ src/pages/myfavorites.js
@@ -3,11 +3,11 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import MyPageComp from "../components/myPage";
 
-const MyPage = () => (
+const MyFavorites = () => (
   <Layout>
     <SEO title="My Page" />
     <MyPageComp />
   </Layout>
 );
 
-export default MyPage;
+export default MyFavorites;
